e6d57ec5a43b62b1460f35e016df879b8789f425
fung <memory852@gmail.com>
2021-03-30 23:41:03 +0800

    Lesson 26

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello world'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Rich'),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://asselum.com/wp-content/uploads/The-Blue-Light-Hazard-according-to-the-CIE-1080x675.jpg'),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
